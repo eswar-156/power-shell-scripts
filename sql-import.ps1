586047dacc29287dcbd4999ce628a8b3f8b2d306
eswar-156 <91057036+eswar-156@users.noreply.github.com>
2021-12-10 13:18:39 +0530

    Update sql-import.ps1

--- sql-import.ps1
+++ sql-import.ps1
@@ -2,6 +2,6 @@
 
 $user = 'eswar'
 $pass = ConvertTo-SecureString "HAPPYboy@123" -AsPlainText  -Force
-$stgaccesskey = Get-AzStorageAccountKey -ResourceGroupName ev-log-rg -Name stg-acc-ev-156
+$stgaccesskey = Get-AzStorageAccountKey -ResourceGroupName ev-log-rg -Name stgaccev156
 
-New-AzSqlDatabaseImport -DatabaseName sql-database-ev-303 -ServerName sql-server-ev-156 -Edition Basic -DatabaseMaxSizeBytes 2GB -StorageKeyType StorageAccessKey  -StorageKey $stgaccesskey.Value[0] -StorageUri 'https://stg-acc-ev-156.blob.core.windows.net/backkuppp/sql-database-ev-156-2021-10-27-14-12.bacpac' -AdministratorLogin $user -AdministratorLoginPassword $pass -ResourceGroupName ev-log-rg -ServiceObjectiveName S0 -AuthenticationType None
+New-AzSqlDatabaseImport -DatabaseName sql-database-ev-303 -ServerName sql-server-ev-156 -Edition Basic -DatabaseMaxSizeBytes 2GB -StorageKeyType StorageAccessKey  -StorageKey $stgaccesskey.Value[0] -StorageUri 'https://stgaccev156.blob.core.windows.net/backkuppp/sql-database-ev-156-2021-10-27-14-12.bacpac' -AdministratorLogin $user -AdministratorLoginPassword $pass -ResourceGroupName ev-log-rg -ServiceObjectiveName S0 -AuthenticationType None
